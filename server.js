960e3c49562ce5030b717975ea12956aa9fe5d6b
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-12-07 13:38:46 +0700

    fix

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +12,16 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+const upload = multer({ dest: "uploads/" });
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res, next) {
+  var upfile = req.file;
+  if (typeof upfile === "undefined") res.json({ error: "file not uploaded" });
+  return res.json({
+    name: upfile.originalname,
+    type: upfile.mimetype,
+    size: upfile.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
